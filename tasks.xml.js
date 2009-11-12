260aafbe3a57dc8eb21b860752d899c3894960d4
tzury <tzury@precision.(none)>
2009-11-12 17:28:45 +0200

    asdas

--- tasks.xml.js
+++ tasks.xml.js
@@ -1,38 +1,38 @@
-var tasklist = window.tasklist = {};
+//~ var tasklist = window.tasklist = {};
 
-tasklist.newId = function () { 
-    lastId = wave.getState().get('lastTaskId', 0);
-    return lastId+1;
-};
+//~ tasklist.newId = function () { 
+    //~ lastId = wave.getState().get('lastTaskId', 0);
+    //~ return lastId+1;
+//~ };
 
-tasklist.Task = function() { return {'title': '', 'done': 0, 'id': tasklist.newId()}; };
-tasklist.Tasklist = function(){ return wave.getState().get('taskList', {});};
+//~ tasklist.Task = function() { return {'title': '', 'done': 0, 'id': tasklist.newId()}; };
+//~ tasklist.Tasklist = function(){ return wave.getState().get('taskList', {});};
 
-tasklist.addNew = function (newTask){ 
-    alert("addNewCalled");
-    list = tasklist.TaskList();
-    list[newTask.taskId] = newTask;
-    wave.getState().submitDelta({'taskList': list});    
-};
+//~ tasklist.addNew = function (newTask){ 
+    //~ alert("addNewCalled");
+    //~ list = tasklist.TaskList();
+    //~ list[newTask.taskId] = newTask;
+    //~ wave.getState().submitDelta({'taskList': list});    
+//~ };
 
-tasklist.Delete = function (taskId) { 
-    alert("DeleteCalled");
-    list = tasklist.TaskList()
-    delete tasklist.TaskList()[taskId];
-    wave.getState().submitDelta({'taskList': list});
-};
+//~ tasklist.Delete = function (taskId) { 
+    //~ alert("DeleteCalled");
+    //~ list = tasklist.TaskList()
+    //~ delete tasklist.TaskList()[taskId];
+    //~ wave.getState().submitDelta({'taskList': list});
+//~ };
 
-tasklist.RenderList = function(){ 
-    alert("RenderCalled");
-    $.each(tasklist.Tasklist(), function(k,v){
-        var newNode = $(
-            "<div class='task'>" 
-            + v.title 
-            + "</div>"
-        );
-        $("#taskList").append(newNode);
-    });
-};
+//~ tasklist.RenderList = function(){ 
+    //~ alert("RenderCalled");
+    //~ $.each(tasklist.Tasklist(), function(k,v){
+        //~ var newNode = $(
+            //~ "<div class='task'>" 
+            //~ + v.title 
+            //~ + "</div>"
+        //~ );
+        //~ $("#taskList").append(newNode);
+    //~ });
+//~ };
 
 function addNewTaskHandler(){    
     var task = tasklist.Task();
