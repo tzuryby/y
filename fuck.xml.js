16b7dc6e70b7b869ea0a7589024510c4ff1a5920
tzury <tzury@israel.(none)>
2009-11-13 07:01:23 +0200

    new tasklist

--- fuck.xml.js
+++ fuck.xml.js
@@ -1,38 +1,38 @@
-            (function () {
-                
-                function _get(){
-                  return eval("(" + wave.getState().get('fuckmehard', '{}') + ")");
-                }
-                function _set(){
-                    wave.getState().submitDelta({'fuckmehard': "{current: Date()}"});
-                }
-                
-                function getGadgetState(){
-                    return wave.getState().get('fuck', '0');
-                }
-                
-                function getStateInt(){                    
-                    return parseInt(getGadgetState());
-                }
-                
-                function updateDom(){
-                    $("#content_div").html(getStateInt());    
-                    alert(wave.getState().toString());                                    
-                }  
+(function () {
+    
+    function _get(){
+      return eval("(" + wave.getState().get('fuckmehard', '{}') + ")");
+    }
+    function _set(){
+        wave.getState().submitDelta({'fuckmehard': "{current: Date()}"});
+    }
+    
+    function getGadgetState(){
+        return wave.getState().get('fuck', '0');
+    }
+    
+    function getStateInt(){                    
+        return parseInt(getGadgetState());
+    }
+    
+    function updateDom(){
+        $("#content_div").html(getStateInt());    
+        alert(wave.getState().toString());                                    
+    }  
 
-                function init() {
-                    if (!wave && !wave.isInWaveContainer()) { return; }
-                    
-                    $(document).ready(function() {
-                        wave.setStateCallback(updateDom);
-                    });
-                    
-                    $("#fuck").click(function(){
-                        _set();
-                        wave.getState().submitDelta({'fuck': getStateInt()+1});
-                    });
-                }
+    function init() {
+        if (!wave && !wave.isInWaveContainer()) { return; }
+        
+        $(document).ready(function() {
+            wave.setStateCallback(updateDom);
+        });
+        
+        $("#fuck").click(function(){
+            _set();
+            wave.getState().submitDelta({'fuck': getStateInt()+1});
+        });
+    }
 
-                gadgets.util.registerOnLoadHandler(init);
+    gadgets.util.registerOnLoadHandler(init);
 
-            })();
+})();
---
+++ tasklist.xml.js
@@ -0,0 +1,41 @@
+(function () {
+    
+    function getGadgetState(){
+        return wave.getState().get('fuck', '[{title:""}]');
+    }
+    
+    function updateDom(){
+        var tasks = $.json.parse(getGadgetState());
+        tasks.each(function(){
+            $("#tasklist").append("<div class='listItem'>" + this.title + "</div>");
+        });
+    }
+    
+    function updateState(){
+        tasks = [];
+        $("div.listItem").each(function(){
+            tasks.append({title: this.html());
+        });
+        wave.getState().submitDelta({'fuck': $.json.stringify(tasks)});
+    }
+    
+    function init() {
+        if (!wave && !wave.isInWaveContainer()) { return; }
+        
+        $(document).ready(function() {
+            wave.setStateCallback(updateDom);
+        });
+        
+        $("#newTaskButton").click(function(){
+            tasks = [];
+            $("div.listItem").each(function(){
+                tasks.append({title: this.html());
+            });
+            tasks.append({title: $("#newTaskTitle").val());
+            wave.getState().submitDelta({'fuck': $.json.stringify(tasks)});
+        });
+    }
+
+    gadgets.util.registerOnLoadHandler(init);
+
+})();
