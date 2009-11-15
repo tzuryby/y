50ddc64f000e744c26c1ea9a0e099015b565c2f9
tzury <tzury@israel.(none)>
2009-11-15 07:26:38 +0200

    o

--- tasklist.xml.js
+++ tasklist.xml.js
@@ -1,5 +1,20 @@
 (function (){
-    
+    if (!wave){
+        wave = {}
+        wave.getState = function() {
+            return {
+                get : function () { 
+                    return {
+                    1: {done:false, title: "do this"},
+                    2: {done:false, title: "do that"}
+                    }
+                },
+                
+                submitDelta: function(){}
+            };
+        };
+        
+    }
     function addTask(task){
         current = wave.getState().get("tasks", '{}');
         current = (current && $.json.parse(current)) || {};
@@ -7,31 +22,39 @@
         current = $.json.stringify(current);
         wave.getState().submitDelta({"tasks": current});
     }
-        
-    function updateDom(){
+    
+    function domifyList(){
         current = wave.getState().get("tasks", '{a:1}');
-        current = (current && $.json.parse(current)) || {};
+        current = (current && $.json.parse(current));
+        
+        // clean tasks from dom
         $("#tasklist").empty();
+        
+        // render list items
         $.each(current, function(){
             $("#tasklist").append(
                 "<div class='task'>" + 
-                "<input type='checkbox'>" +
-                "<input type='text' value='" + this.title + "' />" +
+                    "<input type='checkbox'" + 
+                        ((this.done) ? " checked='checked' " : "") + " />" +
+                    "<input type='text' value='" + 
+                        this.title + "' class='taskInput' />" +
                 "</div>"
             );
         });
-    }  
-
+    }
+    
     function init() {
         if (!wave && !wave.isInWaveContainer()) { return; }
         
         $(document).ready(function() {
-            wave.setStateCallback(updateDom);
+            wave.setStateCallback(domifyList);
         });
         
         $("#newTaskLink").click(function(){
             addTask({title: $("#newTaskText").val()});
         });
     }
+    
     gadgets.util.registerOnLoadHandler(init);
+
 })();
