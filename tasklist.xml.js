2d6c0f7990b66074a85f282076b047227222bc92
tzury <tzury@israel.(none)>
2009-11-16 10:05:02 +0200

    iii

--- tasklist.xml.js
+++ tasklist.xml.js
@@ -29,7 +29,10 @@
     
     function toggleDone(tasks, taskId){
         tasks[taskId].done = !tasks[taskId].done;
-        notify("task#" + taskId + " marked as " + (tasks[taskId].done ? "done" : "not done"));
+        var msg = (tasks[taskId].done) ?  
+            "you are finally done with that " + tasks[taskId].title " thing." :
+            "you are not yet done with it!";
+        notify(msg);
         return tasks;
     }
     
