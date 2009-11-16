b3461cb53c61b4217226053ab4d7440a4351a597
tzury <tzury@israel.(none)>
2009-11-16 14:06:42 +0200

    .slideToggle("slow");

--- tasklist.xml.js
+++ tasklist.xml.js
@@ -138,8 +138,8 @@
         
         var msg = $state("notify");
         if (msg){
-            $("#notificator").html(msg).show();
-            setTimeout(function(){$("#notificator").hide()}, 3000);        
+            $("#notificator").html(msg).slideToggle("slow");
+            setTimeout(function(){$("#notificator").slideToggle("slow");}, 3000);        
         }
     }
     
